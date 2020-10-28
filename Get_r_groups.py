7db1e6f91f66597579a70c45eb3eea2b8e65ee0f
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2020-10-28 14:49:18 +0000

    Started exception handling; notebook to test MaxMinPicker

--- Get_r_groups.py
+++ Get_r_groups.py
@@ -18,12 +18,22 @@ class Get_r_groups:
         self.smiles = smiles
 
     def get_r(self):
-        data = pd.read_csv(self.filename, delimiter=',')
+        # will tidy up exception handling
+        try:
+            data = pd.read_csv(self.filename, delimiter=',')
+        except FileNotFoundError:
+            print("File specified " + self.filename + "does not exist.")
         scaffold = Chem.MolFromSmiles(self.smiles)
-        mols = [Chem.MolFromSmiles(x) for x in data['Smiles']]
+        try:
+            mols = [Chem.MolFromSmiles(x) for x in data['Smiles']]
+        except KeyError:
+            raise RuntimeError("No 'Smiles' column in " + self.filename)
         groups, _ = rdRGD.RGroupDecompose([scaffold], mols, asSmiles=True)
         groups_frame = pd.DataFrame(groups)
-        groups_frame.insert(1, 'Atag', data['Atag'])
-        groups_frame.insert(1, 'Btag', data['Btag'])
-        groups_frame.insert(1, 'pIC50', data['pIC50_MMP12'])
+        try:
+            groups_frame.insert(1, 'Atag', data['Atag'])
+            groups_frame.insert(1, 'Btag', data['Btag'])
+            groups_frame.insert(1, 'pIC50', data['pIC50_MMP12'])
+        except KeyError:
+            raise RuntimeError("R group and pIC50 columns missing from file")
         return groups_frame
