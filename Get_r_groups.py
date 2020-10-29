7b68b50f3a2f9276d189f4422feb515b338c3ae6
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2020-10-29 09:55:45 +0000

    Editing exception handling

--- Get_r_groups.py
+++ Get_r_groups.py
@@ -5,35 +5,34 @@ import pandas as pd
 
 class Get_r_groups:
     """
-    Returns a dataframe containing R groups of a .csv file of molecules.
+    Returns a dataframe containing R groups from a .csv file of molecules.
 
     Parameters
-    :filename: the name of the csv file
-    :filename type: string
-    :smiles: the smiles representation of the molecular scaffold
-    :smiles type: string
+    :param filename: The name of the csv file
+    :type filename: str
+    :param smiles: The smiles representation of the molecular scaffold
+    :type smiles: str
     """
     def __init__(self, filename, smiles):
         self.filename = filename
         self.smiles = smiles
 
     def get_r(self):
-        # will tidy up exception handling
         try:
             data = pd.read_csv(self.filename, delimiter=',')
+            data.columns = map(str.lower, data.columns)
+            scaffold = Chem.MolFromSmiles(self.smiles)
+            mols = [Chem.MolFromSmiles(x) for x in data['smiles']]
+            for col in data.columns:
+                # rename pic50 column to just pic50
+            test_frame = {'Core': mols, 'R1': mols, 'R2': mols}
+            groups_frame = pd.DataFrame(test_frame, columns=['Core', 'R1', 'R2'])
+            for new_col, index in zip(['atag', 'btag', 'pic50'], [1, 3, 5]):
+                groups_frame.insert(index, new_col, data[new_col])
         except FileNotFoundError:
-            print("File specified " + self.filename + "does not exist.")
-        scaffold = Chem.MolFromSmiles(self.smiles)
-        try:
-            mols = [Chem.MolFromSmiles(x) for x in data['Smiles']]
+            print("File specified " + self.filename + " does not exist.")
         except KeyError:
-            raise RuntimeError("No 'Smiles' column in " + self.filename)
-        groups, _ = rdRGD.RGroupDecompose([scaffold], mols, asSmiles=True)
-        groups_frame = pd.DataFrame(groups)
-        try:
-            groups_frame.insert(1, 'Atag', data['Atag'])
-            groups_frame.insert(1, 'Btag', data['Btag'])
-            groups_frame.insert(1, 'pIC50', data['pIC50_MMP12'])
-        except KeyError:
-            raise RuntimeError("R group and pIC50 columns missing from file")
+            raise RuntimeError("No smiles column in " + self.filename)
         return groups_frame
+
+Get_r_groups('spreadsheet2.csv', 'O=S(C1=CC=CC=C1)(NCC(O)=O)=O').get_r()
