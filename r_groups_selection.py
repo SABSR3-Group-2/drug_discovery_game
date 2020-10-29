588b62b7026550304188fc52feb9363128a89b4d
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2020-10-29 14:34:22 +0000

    Modified to return list of tags

--- r_groups_selection.py
+++ r_groups_selection.py
@@ -6,7 +6,8 @@ from rdkit import RDLogger
 
 RDLogger.DisableLog('rdApp.*')
 
-def get_selection(r_group, no_picks, DataSource='data/r_groups_pIC50.csv):
+
+def get_selection(r_group, no_picks, DataSource='data/r_groups_pIC50.csv'):
     data = pd.read_csv(DataSource, delimiter=',')
     r_groups = data[r_group]
     mols = [Chem.MolFromSmiles(mol) for mol in r_groups]
@@ -14,9 +15,9 @@ def get_selection(r_group, no_picks, DataSource='data/r_groups_pIC50.csv):
     picker = rdSimDivPickers.MaxMinPicker()
     picks = list(picker.LazyBitVectorPick(fps, len(fps), no_picks))
     mol_picks = [mols[ind] for ind in picks]
-    
     if r_group == 'R1':
-        return Draw.MolsToGridImage(mol_picks, legends=list(data['Atag'][picks]))
-
+        # return Draw.MolsToGridImage(mol_picks, legends=list(data['Atag'][picks]))
+        return list(data['Atag'][picks])
     elif r_group == 'R2':
-        return Draw.MolsToGridImage(mol_picks, legends=list(data['Btag'][picks]))
+        # return Draw.MolsToGridImage(mol_picks, legends=list(data['Btag'][picks]))
+        return list(data['Btag'][picks])
