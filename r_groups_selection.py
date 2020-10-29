462ae35e22f145f9688553518d1ed8a3c2bf95f6
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2020-10-29 13:51:22 +0000

    Created initial script for displaying r groups

---
+++ r_groups_selection.py
@@ -0,0 +1,25 @@
+from rdkit import Chem
+from rdkit.SimDivFilters import rdSimDivPickers
+from rdkit.Chem import Draw, AllChem
+import pandas as pd
+from rdkit import RDLogger
+
+file_name = 'data/r_groups_pIC50.csv'
+data = pd.read_csv(file_name, delimiter=',')
+RDLogger.DisableLog('rdApp.*')
+
+def get_selection(r_group, no_picks):
+    r_groups = data[r_group]
+    mols = [Chem.MolFromSmiles(mol) for mol in r_groups]
+    fps = [Chem.AllChem.GetMorganFingerprintAsBitVect(x, 2) for x in mols]
+    picker = rdSimDivPickers.MaxMinPicker()
+    picks = list(picker.LazyBitVectorPick(fps, len(fps), no_picks))
+    mol_picks = [mols[ind] for ind in picks]
+    
+    if r_group == 'R1':
+        return Draw.MolsToGridImage(mol_picks, legends=list(data['Atag'][picks]))
+
+    elif r_group == 'R2':
+        return Draw.MolsToGridImage(mol_picks, legends=list(data['Btag'][picks]))
+
+get_selection('R1', 6)
