1f3a09af4b45420ed5d34f349fa4cdadbbb6d645
jamesacb <37155931+jamesbayne@users.noreply.github.com>
2020-11-04 16:47:00 +0000

    added docstrings to r_groups_selection.py

--- game_scripts/r_groups_selection.py
+++ game_scripts/r_groups_selection.py
@@ -5,12 +5,22 @@ import pandas as pd
 
 
 def get_selection(r_group, no_picks, data):
-    r_groups = data[r_group]
+    """Selects a specified number of either R1 or R2 groups from a provided Dataframe of groups.
+
+    :param r_group: either 'R1' or 'R2'
+    :type r_group: string
+    :param no_picks: number of R groups to retrieve
+    :type no_picks: int
+    :param data: Pandas dataframe containing all the R1 and R2 groups
+    :type data: :class:`pandas.Dataframe`
+    :return: [[R group identifiers], [R group SMILES]]
+    :rtype: nested list
+    """
+    r_groups = data[r_group.upper()]  # subset the data to only the given r group (either 'R1' or 'R2')
     mols = [Chem.MolFromSmiles(mol) for mol in r_groups]
-    fps = [Chem.AllChem.GetMorganFingerprintAsBitVect(x, 2) for x in mols]
+    fps = [Chem.AllChem.GetMorganFingerprintAsBitVect(x, 2) for x in mols]  # make fingerprints
     picker = rdSimDivPickers.MaxMinPicker()
     picks = list(picker.LazyBitVectorPick(fps, len(fps), no_picks))
-    mol_picks = [mols[ind] for ind in picks]
     if r_group == 'R1':
         return list(data['atag'][picks]), list(data['R1'][picks])
     elif r_group == 'R2':
