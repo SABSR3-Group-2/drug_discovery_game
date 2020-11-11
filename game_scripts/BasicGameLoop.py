9c9654e009537e066547d9592835ec4976598463
Olivia Simpson <52531289+OliviaSimpson@users.noreply.github.com>
2020-11-11 13:24:26 +0000

    a working initial interface

--- game_scripts/BasicGameLoop.py
+++ game_scripts/BasicGameLoop.py
@@ -18,24 +18,32 @@ class game:
         # self.Options.append(self.getOptions("R1"))
         # self.Options.append(self.getOptions(R2list, "R2"))
         self.rgroupslist = rgroupslist
-        self.currrentchoice = ["", ""]
+        self.currrentchoice = ["",""]
         self.currentoptions = ["", ""]
         self.play()
 
     def play(self,):
         self.Exit = False
         while self.Exit is False:
+            self.currrentchoice = list([])
+            choice = ["",""]
             for group in range(len(self.rgroupslist)):
                 self.currentoptions[group] = self.getOptions(self.rgroupslist[group])
-            self.makeChoice(self.currentoptions[group])
+
+            self.goodcombo = False
+            while self.goodcombo == False:
+                for group in range(len(self.rgroupslist)):
+                    choice[group] = (self.makeChoice(self.currentoptions[group],self.rgroupslist[group]))
+                self.currrentchoice = choice
+                self.molCheck()
             self.giveFeedback()
+            
     
 
     def getOptions(self, GroupSelect="R1"):
         """
 
         """
-
         RList = self.get_selection(GroupSelect)
         try:
             decomp = pd.read_csv(self.decompFile)
@@ -71,16 +79,16 @@ class game:
         rdkit.Chem.PandasTools.AddMoleculeColumnToFrame(rOptions, smilesCol='Smiles', molCol='Mol')
         return rOptions
 
-    def makeChoice(self,options):
+    def makeChoice(self,options,groupname):
+        oppdata = pd.DataFrame(options)
         validChoice = False
         while validChoice == False:
-            for i in range(len(self.rgroupslist)):
-                Rchoice = input("Choice for "+str(self.rgroupslist[i])+" (number only): ")
-                if ("Exit" in Rchoice) or ("exit" in Rchoice) or (Rchoice == "E") or (Rchoice == "e"):
-                    self.Exit = True
-                    return
-                else:
-                    self.currrentchoice[i] = Rchoice
+            Rchoice = input("Choice for "+str(groupname)+" " + str(list(oppdata.loc[:,'tag'])) +": ")                          #add clearer options
+            if ("Exit" in Rchoice) or ("exit" in Rchoice) or (Rchoice == "E") or (Rchoice == "e"):
+                self.Exit = True
+                return
+            else:
+                self.currrentchoice = Rchoice
 
             print("you have selected: " + str(self.currrentchoice))
             self.chosenmolecule = MolChoose(self.currrentchoice[0], self.currrentchoice[1])
@@ -91,22 +99,47 @@ class game:
                 print(self.chosenmolecule)
                 retry = input("do you want to reselect again? [\033[4mY\033[0mes/no]:")
                 if ("Exit" in retry) or ("exit" in retry) or ("No" in retry) or ("no" in retry) or (retry == "E") or (retry == "e"):
-                    self.Exit = True
-                    return
-        return self.chosenmolecule
+                     self.Exit = True
+                     return 
+        return Rchoice
+
+    def molCheck(self):
+        checkcombo = True
+        for choice in self.currrentchoice:
+            goodchoice =False
+            for set in self.currentoptions:
+                if choice in list(set.loc[:,'tag']):
+                    goodchoice = True
+            if goodchoice == False:
+                print("\n\n######################## Oh, that looks wrong ########################\n\""+str(choice)+"\" was not found in the option set, please reselect your R Groups\n######################################################################\n")
+                checkcombo = False
+        self.goodcombo = checkcombo
+        return checkcombo
         
+
+    def UpdateCurrentImage(self, Smiles):
+        print(str(Smiles[1]))
+        # currentmol = rdkit.Chem.MolFromSmiles(str(Smiles))
+        # print(currentmol)
+        # img = Draw.MolsToGridImage(currentmol,  molsPerRow=1, subImgSize=(400, 400))
+        # img.save('Images/CurrentImage.png')
 
 
     def giveFeedback(self):
         # placeholder
         print("giveFeedback() is a place holder")
+        print(self.chosenmolecule)
+        #print("that molecule's pIC50 is "+str(self.chosenmolecule.loc["pIC50"]))
+        # print(self.chosenmolecule)
+        # self.UpdateCurrentImage(self.chosenmolecule.loc["Core"]).values[0]
+        # print(self.chosenmolecule)
 
     def get_selection(self, group):
         #placeholder for full get_selection fucntion
         if group == "R1":
-            return ["A01", "A11", "A21"]
+            return ["A02", "A03", "A04"]                                          #add smiles
         elif group == "R2":
-            return ["B14", "B11", "B21"]
+            return ["B27", "B28", "B29"]
 
 
 game()
