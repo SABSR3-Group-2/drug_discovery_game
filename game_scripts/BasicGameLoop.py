54bb1528fcaa7d48e618d2aa85e4399febf1a006
Olivia Simpson <52531289+OliviaSimpson@users.noreply.github.com>
2020-11-18 14:25:41 +0000

    tidy to flake 8's satisfaction exept line length

--- game_scripts/BasicGameLoop.py
+++ game_scripts/BasicGameLoop.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import rdkit
 from combine import MolChoose
-from Get_r_groups import Get_r_groups
+import os
 from r_groups_selection import get_selection
 from rdkit import Chem
-from rdkit.Chem import PandasTools
 from rdkit.Chem import Draw
-from r_groups_selection import get_selection
-#import pillow
-import os
-_ = os.system('clear') 
+from rdkit.Chem import PandasTools
+# import pillow
+# from Get_r_groups import Get_r_groups
 
-R1list = ["A01", "A11", "A21"]
-R2list = ["B14", "B11", "B21"]
+# clear terminal
+_ = os.system('clear')
 
 
 class game:
@@ -29,12 +27,12 @@ class game:
         self.currentoptions = ["", ""]
         self.play()
 
-    def play(self,termlimit=5):
+    def play(self, termlimit=5):
         self.__scores = []
         self.choiceHistory = []
         self.__loopnumber = 1
         self.Exit = False
-        self.UpdateCurrentImage() 
+        self.UpdateCurrentImage()
         while self.Exit is False:
             print("\n\nRound "+str(self.__loopnumber)+" of "+str(termlimit))
             self.currrentchoice = list([])
@@ -43,17 +41,17 @@ class game:
                 self.currentoptions[group] = self.getOptions(self.rgroupslist[group])
 
             self.goodcombo = False
-            while self.goodcombo == False:
+            while self.goodcombo is False:
                 for group in range(len(self.rgroupslist)):
-                    choice[group] = (self.makeChoice(self.currentoptions[group],self.rgroupslist[group]))
-                    if self.Exit == True:
+                    choice[group] = (self.makeChoice(self.currentoptions[group], self.rgroupslist[group]))
+                    if self.Exit is True:
                         break
-                if self.Exit == True:
+                if self.Exit is True:
                     break
                 self.currrentchoice = choice
                 self.molCheck()
                 self.chosenmolecule = MolChoose(self.currrentchoice[0], self.currrentchoice[1])
-            if self.Exit == True:
+            if self.Exit is True:
                 break
             self.choiceHistory.append(self.currrentchoice)
             self.giveFeedback()
@@ -62,14 +60,11 @@ class game:
                 self.Exit = True
                 self.endgame()
 
-            
-    
-
     def getOptions(self, GroupSelect="R1"):
         """
 
         """
-        #RList = self.get_selection(GroupSelect) #add self. if local
+        # RList = self.get_selection(GroupSelect) #add self. if local
         try:
             decomp = pd.read_csv(self.decompFile)
         except FileNotFoundError:
@@ -77,13 +72,10 @@ class game:
             self.decompFile = input()
         decomp = pd.read_csv(self.decompFile)
 
-        self.Rlist = [get_selection("R1", 3, decomp)[0], get_selection("R2", 3, decomp)[0]] 
+        self.Rlist = [get_selection("R1", 3, decomp)[0], get_selection("R2", 3, decomp)[0]]
         rOptions = self.getSets(decomp, self.Rlist, GroupSelect)
         img = Draw.MolsToGridImage(rOptions["Mol"][:], molsPerRow=len(rOptions["Mol"]), legends=list(rOptions["tag"]))# , subImgSize=(400, 400))
-        #img.show()
         img.save('Images/test'+str(GroupSelect)+'image.png')
-        #import os
-        #os.system('Images/test'+str(GroupSelect)+'image.png')
         return rOptions
 
     def getSets(self, decomp, GroupList, GroupSelect="R1"):
@@ -91,38 +83,27 @@ class game:
 
 
         """
-        rOptions = pd.DataFrame(columns=["tag", "Smiles"])  #setup dataframe
-        if GroupSelect == "R1": #if reading first R group
+        rOptions = pd.DataFrame(columns=["tag", "Smiles"])  # setup dataframe
+        if GroupSelect == "R1":  # if reading first R group
             colName = "atag"
             groupnum = 0
-        elif GroupSelect == "R2": # if reading second R group
+        elif GroupSelect == "R2":  # if reading second R group
             colName = "btag"
             groupnum = 1
 
         for option in range(len(GroupList[groupnum])):
-            # print(GroupList)
-            # print("colls names")
-            # print(decomp[colName])
-            # print("group list options")
-            # print(GroupList[groupnum][option])
-            # print("full thing")
-            # print(decomp.loc[decomp[colName] == GroupList[groupnum][option]])
-            # print("\n")
             commonOtherR = decomp.loc[decomp[colName] == GroupList[groupnum][option]]
             commonOtherR = commonOtherR.iloc[0]
-            # r1mol = Chem.MolFromSmiles(commonOtherR["R1"])
-            # r1mol = Chem.rdmolops.DeleteSubstructs(r1mol, Chem.MolFromSmiles("[H][*]"))
-            # commonOtherR["R1"] = Chem.MolToSmiles(r1mol)
             rAtributes = [commonOtherR[colName], commonOtherR[GroupSelect]]
             rOptions.loc[option] = rAtributes
         rdkit.Chem.PandasTools.AddMoleculeColumnToFrame(rOptions, smilesCol='Smiles', molCol='Mol')
         return rOptions
 
-    def makeChoice(self,options,groupname):
+    def makeChoice(self, options, groupname):
         oppdata = pd.DataFrame(options)
         validChoice = False
-        while validChoice == False:
-            Rchoice = input("> Choice for "+str(groupname)+" " + str(list(oppdata.loc[:,'tag'])) +": ")                          #add clearer options
+        while validChoice is False:
+            Rchoice = input("> Choice for "+str(groupname)+" " + str(list(oppdata.loc[:,'tag'])) + ": ")                          #add clearer options
             if ("Exit" in Rchoice) or ("exit" in Rchoice) or (Rchoice == "E") or (Rchoice == "e"):
                 self.Exit = True
                 return
@@ -138,69 +119,38 @@ class game:
                 print(self.chosenmolecule)
                 retry = input("do you want to reselect again? [\033[4mY\033[0mes/no]:")
                 if ("Exit" in retry) or ("exit" in retry) or ("No" in retry) or ("no" in retry) or (retry == "E") or (retry == "e"):
-                     self.Exit = True
-                     return 
+                    self.Exit = True
+                    return
         return Rchoice
 
     def molCheck(self):
         for groupnum in range(len(self.currrentchoice)):
             checkcombo = True
             checkresults = []
-            if self.currrentchoice[groupnum] in list(self.currentoptions[groupnum].loc[:,"tag"]):
+            if self.currrentchoice[groupnum] in list(self.currentoptions[groupnum].loc[:, "tag"]):
                 checkresults.append(True)
             else:
                 checkresults.append(False)
                 checkcombo = False
-                print("\n\n######################## Oh, that looks wrong ########################\n\""+str(self.currrentchoice[groupnum])+"\" was not found in the option set of options"+str(list(self.currentoptions[groupnum].loc[:,"tag"]))+",\n please reselect your R Groups\nto exit the game type \"Exit\"\n######################################################################\n")
+                print("\n\n######################## Oh, that looks wrong ########################\n\""+str(self.currrentchoice[groupnum])+"\" was not found in the option set of options"+str(list(self.currentoptions[groupnum].loc[:, "tag"]))+",\n please reselect your R Groups\nto exit the game type \"Exit\"\n######################################################################\n")
         self.goodcombo = checkcombo
         return checkcombo
-        
 
     def UpdateCurrentImage(self):
-        # print("- Updatinging current")
-        # print(type(Smiles))
-        #currentmol = Chem.MolFromSmiles(Smiles.to_string(index=False))
-        # print(Smiles.to_string(index=False))
-        # print(Chem.MolFromSmiles(Smiles.to_string(index=False)))
-        # print(currentmol)
-
         from rdkit.Chem.Draw import rdMolDraw2D
         mol = Chem.MolFromSmiles('O=C(O)C(NS(=O)(=O)c1ccc([*:2])cc1)[*:1]  |$;;;;;;;;;;;;R2;;;R1$|')
-        #print(mol.GetAtomWithIdx(3).GetProp("atomLabel"))
-        d = rdMolDraw2D.MolDraw2DCairo(250, 200) # or MolDraw2DSVG to get SVGs
-        #mol.GetAtomWithIdx(15).SetProp('atomNote', 'R1')
-        #mol.GetBondWithIdx(12).SetProp('atomNote', 'R2')
+        d = rdMolDraw2D.MolDraw2DCairo(250, 200)  # or MolDraw2DSVG to get SVGs
         d.drawOptions().addStereoAnnotation = True
-        #d.drawOptions().addAtomIndices = True
         d.DrawMolecule(mol)
         d.FinishDrawing()
         d.WriteDrawingText('Images/CurrentImage.png')
 
-        #img = Chem.Draw.MolToImageFile(currentmol,'Images/CurrentImage.png',includeAtomNumbers=True)
-        #img = Draw.MolsToGridImage(currentmol,  subImgSize=(400, 400))
-        #img.save('Images/CurrentImage.png')
-
-
     def giveFeedback(self):
-        # placeholder
-        #print("- giveFeedback() is a place holder")
-        #print(self.chosenmolecule)
-        pIC50 = self.chosenmolecule.loc[:,"pIC50"].to_string(index=False)
+        pIC50 = self.chosenmolecule.loc[:, "pIC50"].to_string(index=False)
         print("\n- that molecule's pIC50 is "+str(pIC50))
         self.__scores.append(pIC50)
-        # print((self.chosenmolecule))
-        # print(list(self.chosenmolecule.columns) )
-        currentstring = self.chosenmolecule.iloc[:,1]
-        # print(currentstring)
-        self.UpdateCurrentImage() #.values[0]
-
-    # def get_selection(self, group):
-    #     #placeholder for full get_selection fucntion
-    #     if group == "R1":
-    #         return ["A02", "A03", "A04"]                                          #add smiles
-    #     elif group == "R2":
-    #         return ["B27", "B28", "B29"]
-
+        # currentstring = self.chosenmolecule.iloc[:,1]
+        self.UpdateCurrentImage()
 
     def endgame(self):
         print("Thank you for playing")
@@ -210,7 +160,6 @@ class game:
     def plotscores(self):
         import numpy as np
         import matplotlib.pyplot as plt
-        print(self.choiceHistory)
         numericscorelist = []
         fig, ax = plt.subplots()
         for scorenum in range(len(self.__scores)):
@@ -219,11 +168,11 @@ class game:
                 colour = "grey"
                 currentscore = 0.0
                 labelText = "Not Made"
-            elif  currentscore == " Assay Failed":
+            elif currentscore == " Assay Failed":
                 colour = "red"
                 currentscore = 0.0
                 labelText = "Assay Failed"
-            elif  currentscore == " Not Assayed":
+            elif currentscore == " Not Assayed":
                 colour = "red"
                 currentscore = 0.0
                 labelText = "Not Assayed"
@@ -233,17 +182,16 @@ class game:
                 labelText = "Inactive"
 
             else:
-                currentscore = float(str(currentscore).replace(" ",''))
+                currentscore = float(str(currentscore).replace(" ", ''))
                 colour = "limegreen"
                 labelText = "pIC50"
-            print(str(scorenum)+" "+str(currentscore))
             ax.scatter(float(scorenum), float(currentscore), c=colour, label=labelText)
-            ax.annotate(str(self.choiceHistory[scorenum]).replace("\'",''), (scorenum, currentscore))
+            ax.annotate(str(self.choiceHistory[scorenum]).replace("\'", ''), (scorenum, currentscore))
             numericscorelist.append(float(currentscore))
 
-        #ax.legend(False)
-        ax.set_xlim(0,len(numericscorelist))
-        ax.set_ylim(0,max(numericscorelist))
+        # ax.legend(False)
+        ax.set_xlim(0, len(numericscorelist))
+        ax.set_ylim(0, max(numericscorelist))
 
         plt.title('Chosen Molecule pIC50')
         plt.xlabel("Round")
@@ -251,10 +199,9 @@ class game:
         plt.show()
         plt.savefig('Images/CurrentImage.png')
 
-
     def democheat(self):
-        from rdkit import Chem                                                                                                                                                                   
-        from rdkit import RDLogger                                                                                                                                                          
-        RDLogger.DisableLog('rdApp.*') 
+        from rdkit import RDLogger
+        RDLogger.DisableLog('rdApp.*')
+
 
 game()
