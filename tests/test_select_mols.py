524884620c4c877354cc6c6189a79cf7713fd2bd
jamesacb <37155931+jamesbayne@users.noreply.github.com>
2020-12-02 15:39:27 +0000

    disabled logger in get_selection() and wrote two tests for select_mols.py

---
+++ tests/test_select_mols.py
@@ -0,0 +1,26 @@
+"""Tests that the select_mols.py script functions work as expected"""
+
+import pytest
+import pandas as pd
+import os.path
+from game_scripts import select_mols
+
+cwd = os.path.dirname(__file__)  # get current working directory
+
+
+# Test the get selection function
+def test_correct_atag():
+    """Tests that the correct 'tags' are returned given the specified R group. e.g. A1 would be a correct return for R1
+    and B3 for R2 etc."""
+
+    test_data = pd.read_csv(os.path.join(cwd, 'test_get_selec.csv'))
+    test_selection = select_mols.get_selection('R1', 3, test_data)
+    assert (tag[0] == 'A' for tag in test_selection[0])
+
+
+def test_correct_number():
+    """Tests that the number of R groups returned is equal to the specified number"""
+
+    test_data = pd.read_csv(os.path.join(cwd, 'test_get_selec.csv'))
+    test_selection = select_mols.get_selection('R2', 4, test_data)
+    assert len(test_selection[0]) == 4
