0b02f5a4eda23346b77ca5e16b2563ff01465c94
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2020-12-14 09:13:35 +0000

    Updated pains script to filter df

--- game_scripts/pains.py
+++ game_scripts/pains.py
@@ -4,26 +4,41 @@ import pandas as pd
 from rdkit.Chem.FilterCatalog import *
 
 # function to filter a list of molecules
-def pains_filter(mols):
+def pains_filter(smiles):
     """Filters pan-assay interference compounds (PAINS) using RDKit's
     filter catalog.
 
-    :param mols: list of RDKit molecules to filter
+    :param smiles: 
     :type mols: list
 
-    :return: 
-    :rtype:
+    :return: true or false indicating if a PAINS compound
+    :rtype: Boolean value
     """
-    pains_compounds = []
+
     params = FilterCatalogParams()
     params.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS_A)
     params.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS_B)
     params.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS_C)
     catalog = FilterCatalog(params)
-    for mol in mols:
-        if catalog.HasMatch(mol):
-            pains_compounds.append(mol)
-    return pains_compounds
+    mol = Chem.MolFromSmiles(smiles)
+    if catalog.HasMatch(mol):
+        return False
+    else:
+        return True
+
+def apply_filter(df, r_group):
+    """Filters a dataframe for PAINS compounds using the pains_filter function.
+
+    :param df: dataframe containing molecules for filtering
+    :type df: Pandas DataFrame
+    :param r_group: the r_group to run the filtering on
+    :type r_group: string
+
+    :return: filtered dataframe containing non-PAINS compounds
+    :rtype: PandasDataFrame
+    """
+    filtered_df = df[df.apply(lambda x : pains_filter(x[r_group]),axis=1)]
+    return filtered_df
 
 # function to run on a chosen molecule and tell you if/why the molecule is a pains compound
 def pains_check(mol):
