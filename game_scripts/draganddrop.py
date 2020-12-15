4d1f0823ea7b1ba922b482d03f8ec810bc5c52a3
Matthew Holland <mtoholland@gmail.com>
2020-12-15 12:29:53 +0000

    Molecules now disappear when they collide with the main scaffold, have also rearranged the way the R groups are displayed onscreen'

--- game_scripts/draganddrop.py
+++ game_scripts/draganddrop.py
@@ -25,36 +25,9 @@ SCREEN_TITLE = "Go Forth and Discover Drugs"
 CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 
-#Scaffold molecule
-scaffold = Chem.MolFromSmiles('O=S(C1=CC=CC=C1)(NCC(O)=O)=O')
-d = rdMolDraw2D.MolDraw2DCairo(250, 200)
-d.drawOptions().addStereoAnnotation = True
-d.drawOptions().clearBackground = False
-d.DrawMolecule(scaffold)
-d.FinishDrawing()
-d.WriteDrawingText('Images/scaffold.png')
+#Initial scaffold molecule
+scaffold = Chem.MolFromSmiles('O=C(O)C(NS(=O)(=O)c1ccc([*:2])cc1)[*:1] |$;;;;;;;;;;;;R2;;;R1$|')
 
-#fetch n R groups from the dataset
-#Turn decomp R group dataset into a pandas dataframe
-rgroupdata = pd.read_csv('data/r_group_decomp.csv')
-#Get R1 groups
-r1 = get_selection('R1', 3, rgroupdata)
-
-#get R2 groups
-r2 = get_selection('R2', 3, rgroupdata)
-
-#Convert R1 group SMILES to displayable images 
-for r in [r1,r2]:
-    print(r)
-    for idx, list in enumerate(r[1]):
-        rgroup = Chem.MolFromSmiles(list)
-        d = rdMolDraw2D.MolDraw2DCairo(250, 200)
-        d.drawOptions().addStereoAnnotation = True
-        d.drawOptions().clearBackground = False
-        d.DrawMolecule(rgroup)
-        d.FinishDrawing()
-        d.WriteDrawingText('Images/rgroup {}.png'.format(r[0][idx]))
- 
 class MyGame(arcade.Window):
     """ 
     Main game class
@@ -62,7 +35,7 @@ class MyGame(arcade.Window):
 
     def __init__(self):
 
-        #Call the parent class and set up the windwo
+        #Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         #Lists that keep track of the 'sprites' aka molecules and r groups
@@ -77,6 +50,35 @@ class MyGame(arcade.Window):
         """
         This function sets up the game, call it to restart.
         """
+       
+        #scaffold = Chem.MolFromSmiles('O=S(C1=CC=CC=C1)(NCC(O)=O)=O')
+        d = rdMolDraw2D.MolDraw2DCairo(250, 200)
+        d.drawOptions().addStereoAnnotation = True
+        d.drawOptions().clearBackground = False
+        d.DrawMolecule(scaffold)
+        d.FinishDrawing()
+        d.WriteDrawingText('Images/scaffold.png')
+
+        #fetch n R groups from the dataset
+        #Turn decomp R group dataset into a pandas dataframe
+        rgroupdata = pd.read_csv('data/r_group_decomp.csv')
+        #Get R1 groups
+        r1 = get_selection('R1', 3, rgroupdata)
+
+        #get R2 groups
+        r2 = get_selection('R2', 3, rgroupdata)
+
+        #Convert R1 group SMILES to displayable images 
+        for r in [r1,r2]:
+            for idx, list in enumerate(r[1]):
+                rgroup = Chem.MolFromSmiles(list)
+                d = rdMolDraw2D.MolDraw2DCairo(250, 200)
+                d.drawOptions().addStereoAnnotation = True
+                d.drawOptions().clearBackground = False
+                d.DrawMolecule(rgroup)
+                d.FinishDrawing()
+                d.WriteDrawingText('Images/rgroup {}.png'.format(r[0][idx]))
+        
         #Create the sprite lists
         self.scaffold_list = arcade.SpriteList()
         self.rgroup_list = arcade.SpriteList(use_spatial_hash=True)
@@ -86,15 +88,15 @@ class MyGame(arcade.Window):
         self.scaffold_sprite.position = (SCREEN_WIDTH /2,SCREEN_HEIGHT /2)
         self.scaffold_list.append(self.scaffold_sprite)
 
-        #Create and display R1 groups on the left
-        for r in range(len(r1[0])):
-            rgroup = arcade.Sprite('Images/rgroup {}.png'.format(r1[0][r]), TILE_SCALING)
+        #Create and display R2 groups on the left
+        for r in range(len(r2[0])):
+            rgroup = arcade.Sprite('Images/rgroup {}.png'.format(r2[0][r]), TILE_SCALING)
             rgroup.position = (100, r*200 + 100)
             self.rgroup_list.append(rgroup)
         
-        #Create and display R2 groups on the right
-        for r in range(len(r2[0])):
-            rgroup = arcade.Sprite('Images/rgroup {}.png'.format(r2[0][r]), TILE_SCALING)
+        #Create and display R1 groups on the right
+        for r in range(len(r1[0])):
+            rgroup = arcade.Sprite('Images/rgroup {}.png'.format(r1[0][r]), TILE_SCALING)
             rgroup.position = (800, r*200 + 100)
             self.rgroup_list.append(rgroup)
 
@@ -135,6 +137,18 @@ class MyGame(arcade.Window):
         if len(self.held_molecule) == 0:
             return
 
+        #updates on all sprites in play
+        self.scaffold_list.update()
+        self.rgroup_list.update()
+
+        #create a list of any rgroups that have 'collided' with the scaffold
+        snap_on = arcade.check_for_collision_with_list(self.scaffold_sprite, self.rgroup_list)
+        
+        if snap_on != 0: 
+            for r in snap_on:
+                print(r)
+                r.remove_from_sprite_lists()
+
         # We are no longer holding cards
         self.held_molecule = []
 
@@ -145,6 +159,11 @@ class MyGame(arcade.Window):
         for molecule in self.held_molecule:
             molecule.center_x += dx
             molecule.center_y += dy
+
+    def on_update(self, delta_time):
+        """Keeps track of collisions and updates game play"""
+
+        
 
     def on_key_press(self, symbol: int, modifiers: int):
         """ User presses key """
