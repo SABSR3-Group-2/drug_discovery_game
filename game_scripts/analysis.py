c7619902dd1e9788cf5ea9ed0fe057570540b25e
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2021-03-06 10:11:09 +0000

    analysis screen draws max of 12 molecules - can be changed

--- game_scripts/analysis.py
+++ game_scripts/analysis.py
@@ -3,10 +3,18 @@ Analysis view
 """
 
 import arcade
+import pandas as pd
+from game_scripts.combine import MolChoose
+from rdkit import Chem
+from rdkit.Chem import Draw
+import os
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Analysis"
+
+CARD_WIDTH = 130
+CARD_HEIGHT = 100
 
 class AnalysisView(arcade.View):
     """
@@ -17,6 +25,8 @@ class AnalysisView(arcade.View):
         super().__init__()
         self.feedback_view = feedback_view
         self.end_button_list = None
+        self.card_list = None
+        self.mat_list = None
 
         arcade.set_background_color(arcade.color.WHITE)
 
@@ -29,23 +39,58 @@ class AnalysisView(arcade.View):
         # create end button
         self.end_button_list = arcade.SpriteList()
         end_button = arcade.Sprite(f'Images/button_pngs/end_game_blue.png', 0.5)
-        end_button.position = SCREEN_WIDTH - 50, 30
+        end_button.position = SCREEN_WIDTH / 2, SCREEN_HEIGHT - 35
         end_button.name = 'end'
         self.end_button_list.append(end_button)
 
+        # create cards with molecules on them
+        self.card_list = arcade.SpriteList()
+        for index, row in self.feedback_view.final_df.iterrows():
+            mol_info = MolChoose(row['atag'], row['btag'], DataSource=os.path.join('data', 'r_group_decomp.csv')).reset_index(drop=True)
+            mol = Chem.MolFromSmiles(mol_info.at[0, 'mol'])
+            Chem.Draw.MolToFile(mol, 'Images/game_loop_images/selected_mol{}.png'.format(index),
+                            size=(CARD_WIDTH, CARD_HEIGHT), imageType=None)
+            card_sprite = arcade.Sprite('Images/game_loop_images/selected_mol{}.png'.format(index), scale=1)
+            card_sprite.position = self.make_coordinates(index + 1)
+            self.card_list.append(card_sprite)
+
+        # create blank mats for molecules to sit on
+        self.mat_list = arcade.SpriteList()
+        for i in range(1, 13):
+            mat_sprite = arcade.SpriteSolidColor(width = CARD_WIDTH + 5, height = CARD_HEIGHT + 5, color=arcade.color.LIGHT_BLUE)
+            mat_sprite.position = self.make_coordinates(i)
+            self.mat_list.append(mat_sprite)
+
+    def make_coordinates(self, index):
+        """
+        Function to plot the coordinates for the card and mat sprites.
+        """
+        y_slot_height = (SCREEN_HEIGHT / 6)
+        x_slot_1 = CARD_WIDTH / 2 + 10
+        x_slot_2 = SCREEN_WIDTH - (CARD_WIDTH / 2 + 10)
+        if index <= 6:
+            return x_slot_1, y_slot_height * index - y_slot_height / 2
+        else:
+            return x_slot_2, y_slot_height * (index - 6) - y_slot_height / 2
+
+        return x_slot, y_slot
+
     def on_draw(self):
-        """Renders the screen"""
+        """
+        Renders the screen
+        """
         arcade.start_render()
 
-        # draw the end button
+        # draw the sprites
         self.end_button_list.draw()
+        self.mat_list.draw()
+        self.card_list.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
         """
         Called when the user presses a mouse button. Used for determining what happens
         when the user clicks on a button.
         """
-
         # identifies what button the user clicks on
         clicked = arcade.get_sprites_at_point((x, y), self.end_button_list)
         if len(clicked) > 0:  # checks a button has been clicked
