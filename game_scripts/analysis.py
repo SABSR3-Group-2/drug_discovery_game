c12e11c4c0d35ea13fe22bb6aeae6a14d549dc39
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2021-05-03 20:48:51 +0100

    Created cards that display mol and assay info

--- game_scripts/analysis.py
+++ game_scripts/analysis.py
@@ -11,10 +11,50 @@ import os
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
+MENU_WIDTH = int(SCREEN_WIDTH / 3)
 SCREEN_TITLE = "Analysis"
 
 CARD_WIDTH = 130
-CARD_HEIGHT = 100
+CARD_HEIGHT = 70
+MAT_WIDTH = 150
+MAT_HEIGHT = 200
+
+class Card():
+    """
+    Card class to store molecule info and text coordinates
+    """
+
+    def __init__(self, card_coordinates, atag, btag, pic50=None, cl_mouse=None, cl_human=None, logd=None, pampa=None):
+        self.atag = atag
+        self.btag = btag
+        self.x_tag = card_coordinates[0] - 30
+        self.y_tag = card_coordinates[1] + 35
+        self.tag_text = f"{self.atag}, {self.btag}"
+
+        self.pic50 = pic50
+        self.x_pic50 = card_coordinates[0] - 70
+        self.y_pic50 = card_coordinates[1] - 60
+        self.pic50_text = f"pIC50: {self.pic50}"
+        
+        self.cl_mouse = cl_mouse
+        self.x_cl_mouse = card_coordinates[0] - 70
+        self.y_cl_mouse = card_coordinates[1] - 80
+        self.cl_mouse_text = f"Clearance (m): {self.cl_mouse}"
+
+        self.cl_human = cl_human
+        self.x_cl_human = card_coordinates[0] - 70
+        self.y_cl_human = card_coordinates[1] - 100
+        self.cl_human_text = f"Clearance (h): {self.cl_human}"
+
+        self.logd = logd
+        self.x_logd = card_coordinates[0] - 70
+        self.y_logd = card_coordinates[1] - 120
+        self.logd_text = f"Logd: {self.logd}"
+
+        self.pampa = pampa
+        self.x_pampa = card_coordinates[0] - 70
+        self.y_pampa = card_coordinates[1] - 140
+        self.pampa_text = f"PAMPA: {self.pampa}"
 
 class AnalysisView(arcade.View):
     """
@@ -27,8 +67,23 @@ class AnalysisView(arcade.View):
         self.end_button_list = None
         self.card_list = None
         self.mat_list = None
+        self.text_list = []
 
         arcade.set_background_color(arcade.color.WHITE)
+
+        # Make relative units for responsive design
+        self.vw = int(MENU_WIDTH / 4)  # relative width
+        self.vh = int(SCREEN_HEIGHT / 3)  # relative height
+
+        # Used to keep track of our scrolling
+        self.view_top = SCREEN_HEIGHT
+
+        # self.scrolled = 0
+        self.top_bound = 0  # the maximum y value
+        self.bottom_bound = 0  # the minimum y value
+
+        # get font from font file
+        self.font = os.path.join('fonts', 'arial.ttf')
 
         self.setup()
 
@@ -39,41 +94,78 @@ class AnalysisView(arcade.View):
         # create end button
         self.end_button_list = arcade.SpriteList()
         end_button = arcade.Sprite(f'Images/button_pngs/end_game_blue.png', 0.5)
-        end_button.position = SCREEN_WIDTH / 2, SCREEN_HEIGHT - 35
+        end_button.position = SCREEN_WIDTH - 70, 50
         end_button.name = 'end'
         self.end_button_list.append(end_button)
 
         # create cards with molecules on them
         self.card_list = arcade.SpriteList()
         for index, row in self.feedback_view.final_df.iterrows():
+            # get the molecules that have been selected from the df
             mol_info = MolChoose(row['atag'], row['btag'], DataSource=os.path.join('data', 'r_group_decomp.csv')).reset_index(drop=True)
             mol = Chem.MolFromSmiles(mol_info.at[0, 'mol'])
             Chem.Draw.MolToFile(mol, 'Images/game_loop_images/selected_mol{}.png'.format(index),
-                            size=(CARD_WIDTH, CARD_HEIGHT), imageType=None)
-            card_sprite = arcade.Sprite('Images/game_loop_images/selected_mol{}.png'.format(index), scale=1)
-            card_sprite.position = self.make_coordinates(index + 1)
+                            size=(CARD_WIDTH, CARD_HEIGHT), imageType=None)  # save image of the molecule to file
+            card_sprite = arcade.Sprite('Images/game_loop_images/selected_mol{}.png'.format(index), scale=1)  # create sprites of the molecules
             self.card_list.append(card_sprite)
 
         # create blank mats for molecules to sit on
         self.mat_list = arcade.SpriteList()
-        for i in range(1, 13):
-            mat_sprite = arcade.SpriteSolidColor(width = CARD_WIDTH + 5, height = CARD_HEIGHT + 5, color=arcade.color.LIGHT_BLUE)
-            mat_sprite.position = self.make_coordinates(i)
+        for i in range(len(self.card_list)):
+            mat_sprite = arcade.SpriteSolidColor(width = MAT_WIDTH, height = MAT_HEIGHT, color=arcade.color.LIGHT_BLUE)
             self.mat_list.append(mat_sprite)
+        
+        # create coordinates for the cards
+        coordinate_list = self.make_coordinates(len(self.card_list))
+        card_coordinate_list = []
+        for coords in coordinate_list:
+            new_coords = []
+            new_coords.append(coords[0])
+            new_coords.append(coords[1] + 50)
+            card_coordinate_list.append(new_coords)
 
-    def make_coordinates(self, index):
+        # set the coordinates of the molecule and mat sprites
+        for i, sprite in enumerate(self.card_list):
+            sprite.position = card_coordinate_list[i]
+        
+        for i, sprite in enumerate(self.mat_list):
+            sprite.position = coordinate_list[i]
+        
+        # use the Card class to create objects that store the molecule info and coordinates
+        for (index, row), coord in zip(self.feedback_view.final_df.iterrows(), card_coordinate_list):
+            cardtext = Card(coord, row['atag'], row['btag'], row['pic50'], row['cl_mouse'], row['cl_human'], row['logd'], row['pampa'])
+            self.text_list.append(cardtext)
+
+    def make_coordinates(self, n_sprites):
         """
-        Function to plot the coordinates for the card and mat sprites.
+        Function to plot the coordinates for the card sprites.
         """
-        y_slot_height = (SCREEN_HEIGHT / 6)
-        x_slot_1 = CARD_WIDTH / 2 + 10
-        x_slot_2 = SCREEN_WIDTH - (CARD_WIDTH / 2 + 10)
-        if index <= 6:
-            return x_slot_1, y_slot_height * index - y_slot_height / 2
+        coordinate_list = []
+
+        n_cards = len(self.feedback_view.final_df)
+
+        full_rows = int(n_sprites / 2)
+        last_row = n_sprites % 2
+
+        # make full rows
+        for i in range(0, full_rows):
+            vh = SCREEN_HEIGHT - (self.vh * (i + 1))  # the y coordinate for a single row with 3 columns from the top
+            single_row = [[self.vw, vh],
+                          [self.vw * 3, vh]]
+            for c in single_row:  # append each coordinate at a time
+                coordinate_list.append(c)
+
+        # make last row
+        if last_row == 1:
+            coordinate_list.append([self.vw, SCREEN_HEIGHT - self.vh * (full_rows + 1)])
         else:
-            return x_slot_2, y_slot_height * (index - 6) - y_slot_height / 2
+            coordinate_list.append([self.vw, SCREEN_HEIGHT - self.vh * (full_rows + 1)])
+            coordinate_list.append([self.vw * 3, SCREEN_HEIGHT - self.vh * (full_rows + 1)])
 
-        return x_slot, y_slot
+        # save the maximum and minimum y values of the coordinate list
+        self.top_bound = coordinate_list[0][1] + self.vh
+        self.bottom_bound = coordinate_list[-1][1] + full_rows
+        return coordinate_list
 
     def on_draw(self):
         """
@@ -85,6 +177,51 @@ class AnalysisView(arcade.View):
         self.end_button_list.draw()
         self.mat_list.draw()
         self.card_list.draw()
+
+        # draw the text that goes on the cards
+        for c in self.text_list:
+            arcade.draw_text(c.tag_text,
+                        c.x_tag,
+                        c.y_tag,
+                        color=arcade.color.BLACK,
+                        font_size=10,
+                        font_name=self.font,
+                        align='center')
+            arcade.draw_text(c.pic50_text,
+                        c.x_pic50,
+                        c.y_pic50,
+                        color=arcade.color.BLACK,
+                        font_size=10,
+                        font_name=self.font,
+                        align='center')
+            arcade.draw_text(c.cl_mouse_text,
+                        c.x_cl_mouse,
+                        c.y_cl_mouse,
+                        color=arcade.color.BLACK,
+                        font_size=10,
+                        font_name=self.font,
+                        align='center')
+            arcade.draw_text(c.cl_human_text,
+                        c.x_cl_human,
+                        c.y_cl_human,
+                        color=arcade.color.BLACK,
+                        font_size=10,
+                        font_name=self.font,
+                        align='center')
+            arcade.draw_text(c.logd_text,
+                        c.x_logd,
+                        c.y_logd,
+                        color=arcade.color.BLACK,
+                        font_size=10,
+                        font_name=self.font,
+                        align='center')
+            arcade.draw_text(c.pampa_text,
+                        c.x_pampa,
+                        c.y_pampa,
+                        color=arcade.color.BLACK,
+                        font_size=10,
+                        font_name=self.font,
+                        align='center')
 
     def on_mouse_press(self, x, y, button, modifiers):
         """
@@ -104,11 +241,31 @@ class AnalysisView(arcade.View):
         # df containing results printed when user presses SPACE
         if symbol == arcade.key.SPACE:
             print(self.feedback_view.final_df)
+            for i in self.text_list:
+                print(i.y)
 
         if symbol == arcade.key.LEFT:
             self.window.show_view(self.feedback_view)
             arcade.set_background_color(arcade.color.OXFORD_BLUE)
+    
+    def on_mouse_scroll(self, x: int, y: int, scroll_x: int, scroll_y: int):
+        """Redraw the sprites lower instead of scrollling"""
 
+        for i, r in enumerate(self.card_list):
+            r.position = (r.position[0], r.position[1] + scroll_y)
+        for i, r in enumerate(self.mat_list):
+            r.position = (r.position[0], r.position[1] + scroll_y)
+        
+        # change the y position of the card text
+        for c in self.text_list:
+            c.y_tag += scroll_y
+            c.y_pic50 += scroll_y
+            c.y_cl_mouse += scroll_y
+            c.y_cl_human += scroll_y
+            c.y_logd += scroll_y
+            c.y_pampa += scroll_y
+
+        self.view_top += scroll_y
 
 def main():
     """ Main method """
@@ -117,7 +274,6 @@ def main():
     window.show_view(start_view)
     start_view.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
