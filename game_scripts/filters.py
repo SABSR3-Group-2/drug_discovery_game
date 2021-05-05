ec89c582bedfc2cdda0ae8159de5f0b80c96f7fa
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2021-05-05 16:55:35 +0100

    Edited filter results to be more specific

--- game_scripts/filters.py
+++ game_scripts/filters.py
@@ -76,3 +76,47 @@ def compound_check(mol):
         result = 'Molecule passes the filter.'
         results.append(result)
         return results
+
+def run_filters(mol):
+    """Checks whether a molecule is an undesirable compound against each filter and
+    identifies the reason and substructure responsible.
+
+    :param mols: molecule to check
+    :type mols: RDKit molecule
+
+    :return: dictionary containing filter name as key and warnings/passing as values
+    :rtype: dict
+    """
+
+    pains_params = FilterCatalogParams()
+    pains_params.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS_A)
+    pains_params.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS_B)
+    pains_params.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS_C)
+
+    zinc_params = FilterCatalogParams()
+    zinc_params.AddCatalog(FilterCatalogParams.FilterCatalogs.ZINC)
+
+    brenk_params = FilterCatalogParams()
+    brenk_params.AddCatalog(FilterCatalogParams.FilterCatalogs.BRENK)
+
+    nih_params = FilterCatalogParams()
+    nih_params.AddCatalog(FilterCatalogParams.FilterCatalogs.NIH)
+
+    params = [pains_params, zinc_params, brenk_params, nih_params]
+    filt_names = ['PAINS', 'ZINC', 'BRENK', 'NIH']
+
+    all_warnings = {}
+
+    for param, name in zip(params, filt_names):
+        catalog = FilterCatalog(param)
+        entries = catalog.GetMatches(mol)
+        warnings = []
+        for e in entries:
+            warnings.append(e.GetDescription())
+        if len(warnings) != 0:
+            all_warnings[name] = warnings
+        else:
+            all_warnings[name] = 'passing'
+    
+    return all_warnings
+        
